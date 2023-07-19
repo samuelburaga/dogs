da607183701c71d279267b88f402a17e3947b66d
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-19 10:43:12 +0300

    created test components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from './components/second/second.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, FirstComponent, SecondComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
