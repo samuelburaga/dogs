7df79fff4d7e0ce2e901356fae8f14d00d6ba6c4
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-19 10:50:43 +0300

    added new components to routes list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { SecondComponent } from "./components/second/second.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: "first-component", component: FirstComponent },
+	{ path: "second-component", component: SecondComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
