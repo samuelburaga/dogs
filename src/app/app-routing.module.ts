242ebd4905d08ade57854dd19f0e3f373e1359a6
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-19 12:16:04 +0300

    dogs-list component works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { FirstComponent } from "./components/first/first.component";
 import { SecondComponent } from "./components/second/second.component";
+import { DogsListComponent } from "./components/dogs-list/dogs-list.component";
 
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
+	{ path: "dogs-list-component", component: DogsListComponent },
 	{ path: "first-component", component: FirstComponent },
 	{ path: "second-component", component: SecondComponent },
 ];
