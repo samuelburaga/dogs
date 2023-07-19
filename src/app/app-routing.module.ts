d826958dfd1e97744e35f45bb85bfaf32b22c755
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-19 16:42:45 +0300

    remove unused paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,10 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-	{ path: "", redirectTo: "dogs-list", pathMatch: "full" },
 	{ path: "dogs-list-component", component: DogsListComponent },
 	{ path: "first-component", component: FirstComponent },
 	{ path: "second-component", component: SecondComponent },
 	{ path: "breed/:name", component: BreedComponent },
-	{ path: "**", redirectTo: "dogs-list" },
 ];
 
 @NgModule({
