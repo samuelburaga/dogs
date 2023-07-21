d132a3c38d23aed2c4b4cfe3e15d69fbfd7c69fc
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-21 10:42:27 +0300

    all dogs button component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from "./app.component";
 import { DogsListComponent } from "./components/dogs-list/dogs-list.component";
 import { BreedComponent } from "./components/breed/breed.component";
 import { SubBreedComponent } from './components/sub-breed/sub-breed.component';
+import { AllDogsButtonComponent } from './components/all-dogs-button/all-dogs-button.component';
 
 @NgModule({
-	declarations: [AppComponent, DogsListComponent, BreedComponent, SubBreedComponent],
+	declarations: [AppComponent, DogsListComponent, BreedComponent, SubBreedComponent, AllDogsButtonComponent],
 	imports: [BrowserModule, AppRoutingModule, HttpClientModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/components/all-dogs-button/all-dogs-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-dogs-button',
+  templateUrl: './all-dogs-button.component.html',
+  styleUrls: ['./all-dogs-button.component.css']
+})
+export class AllDogsButtonComponent {
+
+}
