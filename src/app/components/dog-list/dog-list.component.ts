3a3bd99823a41fd802fd580382fc935aa39ae9c2
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-22 17:11:02 +0300

    convert list to tree function created

--- src/app/components/dog-list/dog-list.component.ts
+++ src/app/components/dog-list/dog-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { TreeNode } from "primeng/api/treenode";
 import { ApiService } from "src/app/services/api.service";
 
 @Component({
@@ -8,7 +9,7 @@ import { ApiService } from "src/app/services/api.service";
 })
 export class DogListComponent implements OnInit {
 	breedList: string[] = [];
-
+	breedTree: TreeNode[] = [];
 	constructor(private apiService: ApiService) {}
 
 	ngOnInit(): void {
@@ -19,10 +20,29 @@ export class DogListComponent implements OnInit {
 		this.apiService.getAllDogs().subscribe({
 			next: (data) => {
 				this.breedList = Object.keys(data.message);
+				this.convertListToTree(data);
+				// console.log(data);
+				// console.log(this.breedTree);
 			},
 			error: (error) => {
 				console.error("Error fetching dogs list:", error);
 			},
 		});
+	}
+
+	convertListToTree(data: any) {
+		// for (let dog in data.keys) {
+		// 	console.log(dog);
+		// }
+		for (let key in data) {
+			if (typeof data[key] === "object") {
+				for (let nestedKey in data[key]) {
+					console.log(nestedKey);
+					console.log(data[key][nestedKey]);
+				}
+			} else {
+				console.log(key);
+			}
+		}
 	}
 }
